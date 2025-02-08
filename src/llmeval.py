cf83a4031747d9b0ae80b4fc5afa30b44be008f2
BF-DBU <benjamin.fels@student.dbuas.de>
2025-02-08 22:03:39 +0100

    cleaning up notebook

--- src/llmeval.py
+++ src/llmeval.py
@@ -1,5 +1,8 @@
 import json
 import pandas as pd
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def replace_ids_with_sentences(data):
     """
@@ -72,8 +75,10 @@ def clean_json_string(json_string):
     Ein JSON-String ist bspw. nicht korrekt, wenn die geschlossene Klammern fehlen. In diesem Fall wird der JSON-String bis zum letzten gültigen JSON-Objekt extrahiert.
     Das JSON-Objekt wird dann vervollständigt, indem die fehlenden Klammern hinzugefügt werden.
     Beispiel:
+
     JSON-String: 
     "{"ArgumentativeComponents": [{"ID": "1", "Text": "Major Claim 1"}, {"ID": "2","
+    
     Bereinigter JSON-String:
     "{"ArgumentativeComponents": [{"ID": "1", "Text": "Major Claim 1"}]}"
 
@@ -140,3 +145,282 @@ def extract_response_info(response):
     
     return pd.Series([model, system_fingerprint, content, prompt_tokens, completion_tokens, total_tokens, promot_token_details, completion_token_details])
 
+
+def calculate_bleu_score(groundtruth, prediction):
+    """
+    Berechnet den BLEU Score für die Vorhersage im Vergleich zur Grundwahrheit.
+    
+    Args:
+        groundtruth (str): Text der Grundwahrheit.
+        prediction (str): Text der Vorhersage.
+    
+    Returns:
+        score (float): BLEU Score.
+    """
+    # mit split() Sätze in Wörter aufteilen
+    reference_word_list = groundtruth.split()
+    candidate_word_list = prediction.split()
+    # Glättungsfunktion
+    chencherry = SmoothingFunction()
+    # Berechnung BLEU score
+    score = sentence_bleu([reference_word_list], candidate_word_list, smoothing_function=chencherry.method1)
+
+    # Codebausteine zur Implementierung des BLEU Scores entnommen aus: https://www.nltk.org/api/nltk.translate.bleu_score.html
+    
+    return score
+
+
+def calculate_confusion_matrix(actual_list, pred_list, threshold=0.75):
+    """
+    Berechnung der Teile der Confusion Matrix (TP, FN, FP, TN) basierend auf der Ähnlichkeit der Texteabschnitte. 
+    Als Ähnlichkeitsmaß wird der BLEU-Score verwendet. 
+    Jeder Textabschnitt kann nur einmal zugeordnet werden. 
+    Args:
+        actual_list: Liste der Texte aus der Grundwahrheit
+        pred_list: Liste der Texte aus der Vorhersage
+        threshold: Schwellenwert für die Ähnlichkeit (BLEU-Score), um einen Textabschnitt als True Positive zu werten. 
+    Returns:
+    - tp: True Positives
+    - fn: False Negatives
+    - fp: False Positives
+    - tn: True Negatives
+    - similarities: Liste der Ähnlichkeiten der Texteabschnitte
+    
+    Beschreibung:
+    - True Positives (TP): Wenn der vorhergesagte Text mit dem tatsächlichen Text übereinstimmt und die Ähnlichkeit größer oder gleich dem Schwellenwert ist.
+    - False Negatives (FN): Sofern ein Text aus der Grundwahrheit nicht zuvor als TP gewertet wurde, wird er als FN gewertet.
+    - False Positives (FP): Sofern ein Text aus der Vorhersage nicht zuvor als TP gewertet wurde, wird er als FP gewertet.
+    - True Negatives (TN): Da das LLM nur die Argumentationskomponenten vorhersagt und keine nicht-argumentativen Texte, wird TN immer 0 sein.
+    """
+    tp = fn = fp = tn = 0 # Initialisierung des Counters
+    similarities = [] # liste zum speichern der Ähnlichkeiten
+    
+    # Listen in Tupel umwandeln, um die Verwendung der Texte zu verfolgen (Text, Flag für Verwendung)
+    actual_set = {(text, False) for text in actual_list}
+    pred_set = {(text, False) for text in pred_list}
+    
+    # Vergleich der vorhergesagten Texte mit den tatsächlichen Texten 
+    for actual_text, actual_used in actual_set: # Iteration über actual_set, bestehend aus dem Tupel actual_text und actual_used
+        if actual_used:   # Wenn actual_used True ist beteutet es, dass der Text (actual_text) bereits verwendet wurde. 
+            continue    # In diesem Fall wird der Text übersprungen, da bereits zugeordnet.
+        max_similarity = 0 # Initialisierung der maximalen Ähnlichkeit. Wird verwemdet, um herauszufinden, welcher Text aus dem pred_set am besten passt.
+        best_match = None # Initialisierung des besten Übereinstimmung
+        for pred_text, pred_used in pred_set: # Iteration über pred_set, bestehend aus dem Tupel pred_text und pred_used, um den Text mit der höchsten Ähnlichkeit zu finden
+            if pred_used:     # Wenn pred_used True ist beteutet es, dass der Text (pred_text) bereits verwendet wurde.
+                continue    # In diesem Fall wird der Text übersprungen, da bereits zugeordnet.
+            similarity = round(calculate_bleu_score(actual_text, pred_text), 4) # Berechnung der BLEU-Ähnlichkeit vom vorhergesagten Text zum tatsächlichen Text
+            #similarity = are_texts_same(actual_text, pred_text) # Berechnung der Ähnlichkeit vom vorhergesagten Text zum tatsächlichen Text
+            #similarity = jaccard_similarity(set(actual_text.split()), set(pred_text.split())) # Berechnung der Jaccard-Ähnlichkeit vom vorhergesagten Text zum tatsächlichen Text
+            if similarity > max_similarity: # Wenn die Ähnlichkeit größer als die bisherige maximale Ähnlichkeit ist:
+                max_similarity = similarity # Update der maximalen Ähnlichkeit
+                best_match = (pred_text, pred_used) # Update der besten Übereinstimmung
+        similarities.append(max_similarity) # Hinzufügen der maximalen Ähnlichkeit zur Liste
+        
+        if max_similarity >= threshold: # Sofern die maximale Ähnlichkeit größer oder gleich dem Schwellenwert ist:
+            tp += 1  # wird der Text mit der höchsten Ähnlichkeit als True Positive gewertet
+            pred_set.remove(best_match) # Update der Flag für Verwendung, indem der Text aus pred_set entfernt wird und
+            pred_set.add((best_match[0], True))  # neu hinzugefügt wird, jedoch mit der Flag True, um anzuzeigen, dass er bereits verwendet wurde
+            actual_set.remove((actual_text, False)) # wie zuvor für pred_set
+            actual_set.add((actual_text, True)) 
+  
+    # Betrachtung der verbleibenden Texte aus der Grundwahrheit
+    for actual_text, actual_used in actual_set:
+        if not actual_used: # Sofern der Text nicht bereits als TP gewertet wurde, wird er als FN gewertet
+            fn += 1
+
+    # Betrachtung der verbleibenden Texte aus der Vorhersage
+    for pred_text, pred_used in pred_set:
+        if not pred_used: # Sofern der Text nicht bereits als TP gewertet wurde, wird er als FP gewertet
+            fp += 1
+    
+    # TN kann es in diesem Fall nicht geben, da das LLM nur die Argumentationskomponenten vorhersagt und keine nicht-argumentativen Texte. 
+    # Der Wert für TN wird somit bei 0 bleiben.
+
+    return tp, fn, fp, tn, similarities
+
+
+def get_confusion_matrix(row, ground_truth_col: list, prediction_col: list):
+    """
+    Berechnet die Konfusionsmatrix für die Argumentationskomponenten Major Claims, Claims und Premises.
+
+    Args:
+        row: Zeile des Dataframes.
+        ground_truth_col: Liste der Spaltennamen für die Grundwahrheit. Reihenfolge: Major Claims, Claims, Premises.
+        prediction_col: Liste der Spaltennamen für die Vorhersage. Reihenfolge: Major Claims, Claims, Premises.
+
+    Returns:
+        pd.Series: Series mit den Werten für TP, FN, FP, TN und Ähnlichkeit für Major Claims, Claims und Premises
+    """    
+    # Berechnung der Confusion Matrix das erste Paar von Spalten (Major Claims)
+    major_claims_tp, major_claims_fn, major_claims_fp, major_claims_tn, major_claims_sim = calculate_confusion_matrix(row[ground_truth_col[0]], row[prediction_col[0]])
+    # Berechnung der Confusion Matrix für das zweite Paar von Spalten (Claims)
+    claims_tp, claims_fn, claims_fp, claims_tn, claims_sim = calculate_confusion_matrix(row[ground_truth_col[1]], row[prediction_col[1]])
+    # Berechnung der Confusion Matrix für das dritte Paar von Spalten (Premises)
+    premises_tp, premises_fn, premises_fp, premises_tn, premises_sim = calculate_confusion_matrix(row[ground_truth_col[2]], row[prediction_col[2]])
+          
+    # Rückgabe der Werte als Series
+    return pd.Series({
+        'MajorClaims_TP': major_claims_tp,
+        'MajorClaims_FN': major_claims_fn,
+        'MajorClaims_FP': major_claims_fp,
+        'MajorClaims_TN': major_claims_tn,
+        'MajorClaims_Similarity': major_claims_sim,
+        'Claims_TP': claims_tp,
+        'Claims_FN': claims_fn,
+        'Claims_FP': claims_fp,
+        'Claims_TN': claims_tn,
+        'Claims_Similarity': claims_sim,
+        'Premises_TP': premises_tp,
+        'Premises_FN': premises_fn,
+        'Premises_FP': premises_fp,
+        'Premises_TN': premises_tn,
+        'Premises_Similarity': premises_sim,
+    })
+
+
+def calculate_relations_confusion_matrix(actual_list, pred_list, threshold=0.75):
+    """
+    Berechnung von TP, FN, FP und TN für die Argumentationsbeziehungen basierend auf der Ähnlichkeit der Texteabschnitte. Als Ähnlichkeitsmaß wird der BLEU-Score verwendet.
+    Ist vom Prinzip wie die Funktion calculate_confusion_matrix aufgebaut, jedoch mit der Besonderheit, dass die Argumentationsbeziehungen aus Tupeln bestehen.
+    Args:
+        actual_list: Liste der Beziehungen aus der Grundwahrheit.
+        pred_list: Liste der vorhergesagten Beziehungen.
+        threshold: Schwellenwert für die Ähnlichkeit (BLEU-Score) der Texteabschnitte aus den Beziehungen, um als True Positive gewertet zu werden.
+    Returns:
+    - tp: True Positives
+    - fn: False Negatives
+    - fp: False Positives
+    - tn: True Negatives
+    - similarities: Liste der Ähnlichkeiten
+    """
+    tp = fn = fp = tn = 0 # Initialisierung des Counters
+    similarities = [] # Liste zum Speichern der Ähnlichkeiten
+
+    # Ergänzung der Beziehungen um den Flag für Verwendung
+    actual_set = {(origin, stance, target, False) for origin, stance, target in actual_list} 
+    pred_set = {(origin, stance, target, False) for origin, stance, target in pred_list}
+
+    for actual_origin, actual_stance, actual_target, actual_used in actual_set:
+        if actual_used: # sofern das Beziehungs-Tupel für die tatsächlichen Beziehungen bereits verwendet wurde, wird es übersprungen
+            continue
+        max_similarity = (0, 0) # Initialisierung der maximalen Ähnlichkeit als Tupel (origin_similarity, target_similarity)
+        best_match = None # Initialisierung der besten Übereinstimmung
+        for pred_origin, pred_stance, pred_target, pred_used in pred_set:
+            if pred_used: # sofern das Beziehungs-Tupel für die vorhergesagten Beziehungen bereits verwendet wurde, wird es übersprungen
+                continue
+            similarity_origin = round(calculate_bleu_score(actual_origin, pred_origin), 4) # Berechnung der BLEU-Ähnlichkeit für origin und target
+            similarity_target = round(calculate_bleu_score(actual_target, pred_target), 4)
+            similarity = (similarity_origin, similarity_target) # Ähnlichkeit als Tupel (origin_similarity, target_similarity)
+            if similarity > max_similarity: # Wenn die Ähnlichkeit größer als die bisherige maximale Ähnlichkeit ist:
+                max_similarity = similarity
+                best_match = (pred_origin, pred_stance, pred_target, pred_used)
+        similarities.append(max_similarity)
+        
+        if max_similarity[0] >= threshold and max_similarity[1] >= threshold: # Beide Ähnlichkeiten müssen größer oder gleich dem Schwellenwert sein
+            tp += 1 # Wenn das zutrifft, wird der True Positive Counter erhöht
+            pred_set.remove(best_match) # Update der Flag für die Verwendung
+            pred_set.add((best_match[0], best_match[1], best_match[2], True)) 
+            actual_set.remove((actual_origin, actual_stance, actual_target, False)) 
+            actual_set.add((actual_origin, actual_stance, actual_target, True))
+    
+    # Sofern das Beziehungs-Tupel für die tatsächlichen Beziehungen nicht als TP gewertet wurde, wird es als FN gewertet
+    for actual_origin, actual_stance, actual_target, actual_used in actual_set:
+        if not actual_used: 
+            fn += 1
+
+    # Sofern das Beziehungs-Tupel für die vorhergesagten Beziehungen nicht als TP gewertet wurde, wird es als FP gewertet
+    for pred_origin, pred_stance, pred_target, pred_used in pred_set:
+        if not pred_used: 
+            fp += 1
+
+    # Da das LLM keine nicht-argumentativen Texte vorhersagt, wird TN immer 0 sein.
+    tn = 0
+
+    return tp, fn, fp, tn, similarities
+
+def apply_relations_confusion_matrix(row, ground_truth_col: str, prediction_col: str):
+    """
+    Berechnung der Konfusionsmatrix für die Argumentationsbeziehungen.
+    Args:
+        row: Zeile des Dataframes.
+        ground_truth_col: Spaltenname für die Grundwahrheit der Beziehungen.
+        prediction_col: Spaltenname für die Vorhersage der Beziehungen.
+    Returns:
+        pd.Series: Series mit den Werten für TP, FN, FP, TN und Ähnlichkeit für die Beziehungen
+    """
+    tp, fn, fp, tn, similarities = calculate_relations_confusion_matrix(row[ground_truth_col], row[prediction_col])
+    return pd.Series({
+        'Relations_TP': tp,
+        'Relations_FN': fn,
+        'Relations_FP': fp,
+        'Relations_TN': tn,
+        'Relations_Similarity': similarities
+    })
+
+
+def plot_confusion_matrices(dataframe):
+    """
+    Erstellt für jede Reihe in einem DataFrame jeweils eine Konfusionsmatrizen für die Argumentkomponenten und die argumentativen Beziehungen.
+    Args:
+        dataframe (pd.DataFrame): DataFrame mit den Werten der Konfusionsmatrix 
+    """
+    for index, row in dataframe.iterrows():
+        total_tp_mc = row['MajorClaims_TP']
+        total_fn_mc = row['MajorClaims_FN']
+        total_fp_mc = row['MajorClaims_FP']
+        total_tn_mc = row['MajorClaims_TN']
+
+        total_tp_c = row['Claims_TP']
+        total_fn_c = row['Claims_FN']
+        total_fp_c = row['Claims_FP']
+        total_tn_c = row['Claims_TN']
+
+        total_tp_p = row['Premises_TP']
+        total_fn_p = row['Premises_FN']
+        total_fp_p = row['Premises_FP']
+        total_tn_p = row['Premises_TN']
+
+        total_tp_r = row['Relations_TP']
+        total_fn_r = row['Relations_FN']
+        total_fp_r = row['Relations_FP']
+        total_tn_r = row['Relations_TN']
+
+        # Konfusionsmatrizen erstellen
+        confusion_matrix_mc = [[total_tp_mc, total_fn_mc], [total_fp_mc, total_tn_mc]]
+        confusion_matrix_c = [[total_tp_c, total_fn_c], [total_fp_c, total_tn_c]]
+        confusion_matrix_p = [[total_tp_p, total_fn_p], [total_fp_p, total_tn_p]]
+        confusion_matrix_r = [[total_tp_r, total_fn_r], [total_fp_r, total_tn_r]]
+
+        # Plotten der Konfusionsmatrizen
+        fig, axes = plt.subplots(2, 2, figsize=(18, 12))
+
+        sns.heatmap(confusion_matrix_mc, annot=True, fmt='d', cmap='Blues', ax=axes[0, 0])
+        axes[0, 0].set_title(f'{index} - Major Claims')
+        axes[0, 0].set_xlabel('Predicted')
+        axes[0, 0].set_ylabel('Actual')
+        axes[0, 0].set_xticklabels(['True', 'False'])
+        axes[0, 0].set_yticklabels(['True', 'False'])
+
+        sns.heatmap(confusion_matrix_c, annot=True, fmt='d', cmap='Blues', ax=axes[0, 1])
+        axes[0, 1].set_title(f'{index} - Claims')
+        axes[0, 1].set_xlabel('Predicted')
+        axes[0, 1].set_ylabel('Actual')
+        axes[0, 1].set_xticklabels(['True', 'False'])
+        axes[0, 1].set_yticklabels(['True', 'False'])
+
+        sns.heatmap(confusion_matrix_p, annot=True, fmt='d', cmap='Blues', ax=axes[1, 0])
+        axes[1, 0].set_title(f'{index} - Premises')
+        axes[1, 0].set_xlabel('Predicted')
+        axes[1, 0].set_ylabel('Actual')
+        axes[1, 0].set_xticklabels(['True', 'False'])
+        axes[1, 0].set_yticklabels(['True', 'False'])
+
+        sns.heatmap(confusion_matrix_r, annot=True, fmt='d', cmap='Blues', ax=axes[1, 1])
+        axes[1, 1].set_title(f'{index} - Relations')
+        axes[1, 1].set_xlabel('Predicted')
+        axes[1, 1].set_ylabel('Actual')
+        axes[1, 1].set_xticklabels(['True', 'False'])
+        axes[1, 1].set_yticklabels(['True', 'False'])
+
+        plt.tight_layout()
+        plt.show()
